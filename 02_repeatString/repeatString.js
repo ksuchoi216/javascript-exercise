937ff6a02ac43dd538f2a18ffe0e0d381ec9a856
KC <53473769+ksuchoi216@users.noreply.github.com>
2021-12-27 16:37:19 +0000

    first update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let output='';
+    while(num != 0){
+        output+=word;
+        num-=1
+        console.log(num)
+    }
+    // console.log(output)s
+    return output
 };
-
+// repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
