842995fef0735e080b2dcbb5fb77332d67c07b9b
KC <53473769+ksuchoi216@users.noreply.github.com>
2021-12-31 01:18:28 +0000

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arrs) {
+  if (arrs.length === 0){
+    return 0;
+  }
+
+  let sum = arrs.reduce(function (total, current){
+    return total + current;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arrs) {
+  if (arrs.length === 0){
+    return 0;
+  }
 
+  let multiply = arrs.reduce(function (total, current){
+    return total * current;
+  });
+
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0){
+    return -1;
+  } else if (num == 0){
+    return 1;
+  } else {
+    return (num * factorial(num-1));
+  }
 };
+//calling test
+// console.log(factorial(3));
+
 
 // Do not edit below this line
 module.exports = {
